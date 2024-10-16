c711740250b4f1424ad6dadce6f3187b36ecdfd7
asma-benali-dz <asma18benali@gmail.com>
2024-10-16 20:00:59 +0100

    Add files via upload

---
+++ tb1.ca.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[ROWS][COLS] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    printf("Original matrix:\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+
+    return 0;
+}
